9dd34603ce126cfb428b1b443ce13a2ba4cf39d4
mayershohag <shohagsreepur2021@gmail.com>
2024-10-26 20:23:03 +0600

    test.js file practiced

---
+++ test.js
@@ -0,0 +1,69 @@
+// class Hello{
+//     constructor(name){
+//         this.name = name;
+//         // this.r = r;
+//     }
+//     h(){
+//         return `my name is ${this.name}`
+//     }
+// }
+// const obj = new Hello("Shohag", 34);
+// const obj2 = new Hello("Shohag", 34);
+// console.log(obj.h());
+// console.log(obj2);
+
+
+
+// let a = new Object();
+// console.log(a instanceof Object);
+
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+
+// function Person(name, age) {
+//     if (!new.target) {
+//         throw Error("Please Use 'new Keyword' ")
+//     }
+//     this.name = name;
+//     this.age = age;
+// }
+
+
+// let result = new Person("Shohag", 20);
+// console.log(result);
+
+
+
+// Array Merge 
+// const arr = [1, 2];
+// const copiedArr = [1, 2, 3, 4, 5];
+// let temp = [...arr, ...copiedArr]
+
+
+// Object Copy 
+// const obj = { name: "Shohag", age: 20 };
+// const coppiedObj = {...obj};
+// console.log(coppiedObj.age);
+
+
+
+// Object Merge 
+// const obj = { name: "Shohag", age: 20 };
+// const obj2 = { city: "Pabna", Zip: 6600 };
+
+// let temp = {...obj, ...obj2};
+// console.log(temp);
+
+
+
+
