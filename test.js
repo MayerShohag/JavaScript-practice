7fa9505a1308f8a3035c484b2a5e4136035f0911
mayershohag <shohagsreepur2021@gmail.com>
2024-10-27 19:30:28 +0600

    javascript practice

--- test.js
+++ test.js
@@ -12,8 +12,6 @@
 // console.log(obj.h());
 // console.log(obj2);
 
-
-
 // let a = new Object();
 // console.log(a instanceof Object);
 
@@ -29,7 +27,6 @@
 //     }
 // }
 
-
 // function Person(name, age) {
 //     if (!new.target) {
 //         throw Error("Please Use 'new Keyword' ")
@@ -38,32 +35,47 @@
 //     this.age = age;
 // }
 
-
 // let result = new Person("Shohag", 20);
 // console.log(result);
 
-
-
-// Array Merge 
+// Array Merge
 // const arr = [1, 2];
 // const copiedArr = [1, 2, 3, 4, 5];
 // let temp = [...arr, ...copiedArr]
 
-
-// Object Copy 
+// Object Copy
 // const obj = { name: "Shohag", age: 20 };
 // const coppiedObj = {...obj};
 // console.log(coppiedObj.age);
 
-
-
-// Object Merge 
+// Object Merge
 // const obj = { name: "Shohag", age: 20 };
 // const obj2 = { city: "Pabna", Zip: 6600 };
 
 // let temp = {...obj, ...obj2};
 // console.log(temp);
 
+class Hello {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
 
+class hi extends Hello {
+  constructor(name, age, grade, dept) {
+    super(name, age);
+    this.grade = grade;
+    this.dept = dept;
+  }
+}
 
+class hey extends hi {
+  constructor(name, age, grade, dept, city) {
+    super(name, age, grade, dept, city);
+    this.city = city;
+  }
+}
 
+const result = new hey("Shohag", 20, "12th", "Computer", "Pabna");
+console.log(result);
