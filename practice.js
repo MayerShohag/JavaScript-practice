8d3389b188ff7ebfae7d612005dd7f70c0701de5
mayershohag <shohagsreepur2021@gmail.com>
2024-10-29 20:49:53 +0600

    practice.js file updated

---
+++ practice.js
@@ -0,0 +1,58 @@
+// const data = [
+//   { id: 10, name: "Apple", type: "Fruit", price: 1.2 },
+//   { id: 2, name: "Toyota", type: "Car", year: 2020 },
+//   { id: 32, name: "Tesla", type: "User", age: 28 },
+//   { id: 14, name: "sumon", type: "Book", author: "George Orwell" },
+//   { id: 5, name: "Inception", type: "Movie", director: "Christopher Nolan" },
+//   { id: 6, name: "Samsung", type: "Electronic", category: "Phone" },
+//   { id: 64, name: "Tesla", type: "Car", year: 2023 },
+//   { id: 8, name: "Jane Smith", type: "User", age: 34 },
+// ];
+
+// ["Apple", "Toyota", ""]
+// let arr = data.reduce( (total, el) =>{
+//     total.push(el.name)
+//     return total;
+// }, [])
+
+// console.log(arr);
+
+// class Hello{
+//   static hi(a, b){
+//     return a + b;
+//   }
+// }
+// console.log(Hello.hi(3, 5));
+
+// let arr = [1, 2, 3, 4];
+// let arr2 = [5, 6, 7, 8];
+// let temp = [...arr, ...arr2];
+// console.log(temp);
+
+// const arr = [1, 2, 3, 5, 6, 7];
+// function passValue(...arr) {
+//   arr.reduce((sum, el) => {
+//     // sum = sum + el;
+//     // return sum;
+//     return (sum += el);
+//   }, 0);
+// }
+// console.log(passValue(1, 2, 3, 45));
+
+
+
+
+const object = {
+  name : "Shohag",
+  age : 20,
+  dept : "Computer",
+  session : "2021-22",
+}
+
+let string = JSON.stringify(object);
+console.log(string);
+
+
+let result = JSON.parse(string);
+console.log(result);
+
